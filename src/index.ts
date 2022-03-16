b8ae227848654b2e0925ca0f2216f914499fb0cb
EC2 Default User <gillithebeast@gmail.com>
2022-03-16 09:39:26 +0000

    adding cors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 
 const port = 8080;
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
     res.status(200).send('Welcome to the unofficial exalted api');
