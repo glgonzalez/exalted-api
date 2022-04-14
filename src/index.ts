b2f3ae1e7fd1543302f9495606be6f92369b01c1
Gilliano Gonzalez <glgonzalez1@gmail.com>
2022-04-14 01:57:34 -0500

    removing unused dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import healthCheckRouter from './routes/health-check';
 import connect from './utils/database';
-import usersRouter from './routes/users';
-import authenticationRouter from './routes/authentication';
 import routes from './routes';
 
 dotenv.config();
